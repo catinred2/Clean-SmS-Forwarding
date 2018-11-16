63100d7540460b6f096f97506c56c70ad20506f4
xming <mgabcde@gmail.com>
2018-11-16 10:53:06 +0800

    优化长短信

--- app/src/main/java/cc/mightu/sms_forward/SmsBroadcastReceiver.java
+++ app/src/main/java/cc/mightu/sms_forward/SmsBroadcastReceiver.java
@@ -17,7 +17,7 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
         String action = intent.getAction();
         if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(action)) {
 
-            Log.i("sms", "on receive," + intent.getAction());
+            Log.i(Constants.LOG_TAG, "on receive," + intent.getAction());
 
             String number = context.getSharedPreferences("data", Context.MODE_PRIVATE).getString("number", "");
             if (number == "") {
@@ -25,24 +25,23 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
                 return;
             }
 
-            for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+
+            SmsMessage[] smsList = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+            if (smsList == null || smsList.length <= 0){
+                Log.e(Constants.LOG_TAG,"parse Sms Message From Intent Error");
+                return;
+            }
+            String address = smsList[0].getOriginatingAddress();
+            StringBuilder message = new StringBuilder();
+            message.append("[").append(address).append("]");
+            for (SmsMessage smsMessage : smsList) {
 
                 String messageBody = smsMessage.getMessageBody();
-                String emailFrom = smsMessage.getEmailFrom();
-                String address = smsMessage.getOriginatingAddress();
-                Log.i(Constants.LOG_TAG, "body: " + messageBody);
-                Log.i(Constants.LOG_TAG, "address: " + address);
-
-                String message = "[" + address + "] " + messageBody;
-
-
-                Log.i(Constants.LOG_TAG, "sending to " + number);
-
-                Log.i(Constants.LOG_TAG, "message send:" + message);
-                SmsManager sms = SmsManager.getDefault();
-                ArrayList<String> dividedMessages = sms.divideMessage(message);
-                sms.sendMultipartTextMessage(number, null, dividedMessages, null, null);
+                message.append(messageBody);
             }
+            SmsManager sms = SmsManager.getDefault();
+            ArrayList<String> dividedMessages = sms.divideMessage(message.toString());
+            sms.sendMultipartTextMessage(number, null, dividedMessages, null, null);
 
         } else if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
             Intent startIntent = new Intent(context, MainActivity.class);
